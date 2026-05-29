5260eea753c4a9f90d487f820ce53a5d9b0dde40
premkaanth <premkaanth2000@gmail.com>
2026-05-29 21:12:15 +0530

    Tester1

---
+++ src/test/java/com/omrbranch/runner/LoginPage.java
@@ -0,0 +1,12 @@
+package com.omrbranch.runner;
+
+public class LoginPage {
+	public void emailAddress() {
+		System.out.println("Enter email here");
+	}
+	
+	public void password() {
+		System.out.println("Enter password here");
+	}
+
+}
